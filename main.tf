8a3e471b939dacec381b9262bc48c652609c20b5
nithish-ravindra <nithish.ravindra@hashicorp.com>
2025-01-25 19:41:33 +0530

    test commit

--- main.tf
+++ main.tf
@@ -1,6 +1,27 @@
+# data "aws_vpc" "selected" {
+#   filter {
+#     name   = "tag:om"
+#     values = ["zync"]
+#   }
+# }
+
 data "aws_vpc" "selected" {
   filter {
     name   = "tag:om"
     values = ["zync"]
+  }
+}
+
+data "aws_subnet_ids" "selected" {
+  vpc_id = data.aws_vpc.selected.id
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet_ids.selected.ids[0]
+
+  tags = {
+    Name = "example-instance"
   }
 }
