13448d62d4e30d56b52d57bdba13c3de13fb2d26
Nithish R <nithish.ravindra@hashicorp.com>
2025-02-03 13:04:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -176,6 +176,12 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  
   }
+  ingress {
+    from_port   = 23
+    to_port     = 23
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
 
   egress {
     from_port   = 0
