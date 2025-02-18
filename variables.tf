a0ccc075b0b3480fd10a368a3581e76e4f8b7a43
Nithish R <nithish.ravindra@hashicorp.com>
2025-02-18 22:29:51 +0530

    test

--- variables.tf
+++ variables.tf
@@ -4,17 +4,6 @@ variable "aws_region" {
 }
 
 variable "release_version" {
-<<<<<<< Updated upstream
-    description = "The release version of the deployment"
-    type        = string
-    default     = "us-east-1" 
-}
-
-variable "env" {
-    description = "The environment for the resources"
-    type        = string
-}
-=======
   description = "The release version of the deployment"
   type        = string
 }
@@ -23,4 +12,3 @@ variable "env" {
   description = "The environment for the resources"
   type        = string
 }
->>>>>>> Stashed changes
