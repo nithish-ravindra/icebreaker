5ac03a223dbdef427fdf280cd1daa2bfb4e6dd95
Nithish R <nithish.ravindra@hashicorp.com>
2025-02-18 22:28:25 +0530

    test

--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
 variable "aws_region" {
-    description = "The AWS region to deploy resources."
-    default     = "us-east-1" 
+  description = "The AWS region to deploy resources."
+  default     = "us-east-1"
 }
 
 variable "release_version" {
+<<<<<<< Updated upstream
     description = "The release version of the deployment"
     type        = string
     default     = "us-east-1" 
@@ -13,3 +14,13 @@ variable "env" {
     description = "The environment for the resources"
     type        = string
 }
+=======
+  description = "The release version of the deployment"
+  type        = string
+}
+
+variable "env" {
+  description = "The environment for the resources"
+  type        = string
+}
+>>>>>>> Stashed changes
